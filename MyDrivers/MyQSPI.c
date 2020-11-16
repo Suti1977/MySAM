9f8301ba2266518054665d12d49608bf571084a2
Sütő István <suti1977@gmail.com>
2020-11-16 20:46:38 +0100

    MyQSPI QSPI driver implementálása

---
+++ MyDrivers/MyQSPI.c
@@ -0,0 +1,132 @@
+//------------------------------------------------------------------------------
+//  QSPI driver
+//
+//    File: MyQSPI.c
+//------------------------------------------------------------------------------
+#include "MyQSPI.h"
+#include <string.h>
+#include "MyGCLK.h"
+//------------------------------------------------------------------------------
+void MyQSPI_init(MyQSPI_t* dev, const MyQSPI_config_t* cfg)
+{
+    //Driver valtozoinak nullazasa
+    memset(dev, 0, sizeof(MyQSPI_t));
+
+    dev->hw=cfg->hw;
+    Qspi* hw=cfg->hw;
+
+    //periferia orajel engedelyezese
+    MY_ENTER_CRITICAL();
+    MCLK->AHBMASK.bit.QSPI_=1;
+    MCLK->AHBMASK.bit.QSPI_2X_=1;
+    MCLK->APBCMASK.bit.QSPI_=1;
+    MY_LEAVE_CRITICAL();
+
+    //Periferia szoftveres resetelese
+    hw->CTRLA.bit.SWRST=1;
+    while(hw->CTRLA.bit.SWRST);
+
+    hw->CTRLB.reg=
+            //Uzemmod kivalasztasa. A Driver memoria eleresi modot tamogat!
+            QSPI_CTRLB_MODE_MEMORY |
+            //Az utolso atvitelt a LATXFER bittel jelzi a driver
+            QSPI_CTRLB_CSMODE_LASTXFER |
+            //8 bites adatokkal operalunk
+            QSPI_CTRLB_DATALEN(QSPI_CTRLB_DATALEN_8BITS_Val) |
+            //Delay Between Consecutive Transfers
+            QSPI_CTRLB_DLYBCT(cfg->dlyBCT) |
+            //Minimum Inactive CS Delay
+            QSPI_CTRLB_DLYCS(cfg->dlyCS);
+
+    hw->CTRLB.reg=
+            ((cfg->cpol) ? QSPI_BAUD_CPOL : 0) |
+            ((cfg->cpha) ? QSPI_BAUD_CPHA : 0) |
+            QSPI_BAUD_BAUD(cfg->baudValue) |
+            //Delay Before SCK
+            QSPI_BAUD_DLYBS(cfg->dlySC);
+
+    //QSPI periferia engedelyezese
+    hw->CTRLA.bit.ENABLE=1;
+}
+//------------------------------------------------------------------------------
+//QSPI periferian adat transzfer
+status_t MyQSPI_transfer(MyQSPI_t* dev, const MyQSPI_cmd_t* cmd)
+{
+    Qspi* hw=dev->hw;
+
+    if (cmd->instFrame.bits.addr_en)
+    {   //Elo van irva, hogy cim mezo is tartozik az uzenethez. Beallitjuk.
+        hw->INSTRADDR.reg=cmd->address;
+    }
+
+    uint32_t tmp = hw->INSTRCTRL.reg;
+    if (cmd->instFrame.bits.inst_en)
+    {   //Elo van irva, hogy parancs mezo is tartozik az uzenethez. Beallitjuk.
+        tmp &= ~QSPI_INSTRCTRL_INSTR_Msk;
+        tmp |= QSPI_INSTRCTRL_INSTR((uint32_t)cmd->instruction);
+        ((Qspi *)hw)->INSTRCTRL.reg = tmp;
+    }
+
+    if (cmd->instFrame.bits.opt_en)
+    {   //Elo van irva, option parancs mezo is tartozik az uzenethez. Beallitjuk.
+        tmp &= ~QSPI_INSTRCTRL_OPTCODE_Msk;
+        tmp |= QSPI_INSTRCTRL_OPTCODE((uint32_t)  cmd->option);
+    }
+    hw->INSTRCTRL.reg=tmp;
+
+    //Instrukcio frame regiszter beallitasa. A cmd.ben talalhato bitmezo fele-
+    //pitese megegyezik a regiszter bitjeinek a felepitesevel, igy abba az
+    //azonnal beirhato.
+    hw->INSTRFRAME.reg=cmd->instFrame.word;
+
+    if (cmd->instFrame.bits.data_en)
+    {   //Van adattartalom mozgatas is.
+
+        //Cim kiszamitasa...
+        uint8_t *qspiMemPtr = (uint8_t *)QSPI_AHB;
+        if (cmd->instFrame.bits.addr_en)
+        {
+            qspiMemPtr += cmd->address;
+        }
+
+        //Szinkronizacio a rendszerbuszhoz, az INSTRFRAME regiszter olvasasaval
+        volatile uint32_t dumy=hw->INSTRFRAME.reg;
+        (void) dumy;
+
+        ASSERT(cmd->txBuf || cmd->rxBuf);
+
+        uint32_t leftByte=cmd->bufLen;
+        //irany szerinti adatmozgatas...
+        if (cmd->txBuf)
+        {   //Van kimeneti buffer. Kifele irunk...
+            const uint8_t* src=(const uint8_t*)cmd->txBuf;
+            for(;leftByte; leftByte--)
+            {
+                *qspiMemPtr++ = *src++;
+            }
+        } else
+        {   //Olvasunk...
+            uint8_t* dst=(uint8_t*)cmd->rxBuf;
+            for(;leftByte; leftByte--)
+            {
+                *dst++ = *qspiMemPtr++;
+            }
+        }
+        __DSB();
+        __ISB();
+
+    } //if (cmd->inst_frame.bits.data_en)
+
+    //transzfer lezarasa. (A driver ugy van beallitva, hogy a tranzakcio leza-
+    //rasat a LASTXFER bit-el jelezzuk
+    hw->CTRLA.bit.LASTXFER=1;
+
+    //Varakozas az utasitas befejezesere
+    while(hw->INTFLAG.bit.INSTREND==0);
+    //IT flag torlese...
+    hw->INTFLAG.reg=QSPI_INTFLAG_INSTREND;
+
+    return kStatus_Success;
+}
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
---
+++ MyDrivers/MyQSPI.h
@@ -0,0 +1,149 @@
+//------------------------------------------------------------------------------
+//  QSPI driver
+//
+//    File: MyQSPI.h
+//------------------------------------------------------------------------------
+#ifndef MYQSPI_H_
+#define MYQSPI_H_
+
+#include "MyCommon.h"
+
+
+//Qspi access modes
+enum
+{
+    //Read access
+    MYQSPI_READ_ACCESS=QSPI_INSTRFRAME_TFRTYPE_READ_Val,
+    //Read memory access
+    MYQSPI_READMEM_ACCESS=QSPI_INSTRFRAME_TFRTYPE_READMEMORY_Val,
+    //Write access
+    MYQSPI_WRITE_ACCESS=QSPI_INSTRFRAME_TFRTYPE_WRITE_Val,
+    //Write memory access
+    MYQSPI_WRITEMEM_ACCESS=QSPI_INSTRFRAME_TFRTYPE_WRITEMEMORY_Val
+};
+
+//QSPI command instruction/address/data width
+enum
+{
+    //Instruction: Single-bit, Address: Single-bit, Data: Single-bit
+    MYQSPI_INST1_ADDR1_DATA1=QSPI_INSTRFRAME_WIDTH_SINGLE_BIT_SPI_Val,
+    //Instruction: Single-bit, Address: Single-bit, Data: Dual-bit
+    MYQSPI_INST1_ADDR1_DATA2=QSPI_INSTRFRAME_WIDTH_DUAL_OUTPUT_Val,
+    //Instruction: Single-bit, Address: Single-bit, Data: Quad-bit
+    MYQSPI_INST1_ADDR1_DATA4=QSPI_INSTRFRAME_WIDTH_QUAD_OUTPUT_Val,
+    //Instruction: Single-bit, Address: Dual-bit, Data: Dual-bit
+    MYQSPI_INST1_ADDR2_DATA2=QSPI_INSTRFRAME_WIDTH_DUAL_IO_Val,
+    //Instruction: Single-bit, Address: Quad-bit, Data: Quad-bit
+    MYQSPI_INST1_ADDR4_DATA4=QSPI_INSTRFRAME_WIDTH_QUAD_IO_Val,
+    //Instruction: Dual-bit, Address: Dual-bit, Data: Dual-bit
+    MYQSPI_INST2_ADDR2_DATA2=QSPI_INSTRFRAME_WIDTH_DUAL_CMD_Val,
+    //Instruction: Quad-bit, Address: Quad-bit, Data: Quad-bit
+    MYQSPI_INST4_ADDR4_DATA4=QSPI_INSTRFRAME_WIDTH_QUAD_CMD_Val
+};
+
+
+//QSPI command option code length in bits
+enum
+{
+    //The option code is 1 bit long
+    MYQSPI_OPT_1BIT=QSPI_INSTRFRAME_OPTCODELEN_1BIT_Val,
+    //The option code is 2 bits long
+    MYQSPI_OPT_2BIT=QSPI_INSTRFRAME_OPTCODELEN_2BITS_Val,
+    //The option code is 4 bits long
+    MYQSPI_OPT_4BIT=QSPI_INSTRFRAME_OPTCODELEN_4BITS_Val,
+    //The option code is 8 bits long
+    MYQSPI_OPT_8BIT=QSPI_INSTRFRAME_OPTCODELEN_8BITS_Val
+};
+//------------------------------------------------------------------------------
+//QSPI-n torteno parancsot leiro struktura
+typedef struct
+{
+    union
+    {
+        struct
+        {
+            /* Width of QSPI Addr , inst data */
+            uint32_t width : 3;
+            /* Reserved */
+            uint32_t reserved0 : 1;
+            /* Enable Instruction */
+            uint32_t inst_en : 1;
+            /* Enable Address */
+            uint32_t addr_en : 1;
+            /* Enable Option */
+            uint32_t opt_en : 1;
+            /* Enable Data */
+            uint32_t data_en : 1;
+            /* Option Length */
+            uint32_t opt_len : 2;
+            /* Address Length */
+            uint32_t addr_len : 1;
+            /* Option Length */
+            uint32_t reserved1 : 1;
+            /* Transfer type */
+            uint32_t tfr_type : 2;
+            /* Continuous read mode */
+            uint32_t continues_read : 1;
+            /* Enable Double Data Rate */
+            uint32_t ddr_enable : 1;
+            /* Dummy Cycles Length */
+            uint32_t dummy_cycles : 5;
+            /* Reserved */
+            uint32_t reserved3 : 11;
+        } bits;
+        uint32_t word;
+    } instFrame;
+
+    uint8_t  instruction;
+    uint8_t  option;
+    uint32_t address;
+
+    size_t      bufLen;
+    const void* txBuf;
+    void*       rxBuf;
+} MyQSPI_cmd_t;
+//------------------------------------------------------------------------------
+//QSPI drivert inicializalo struktura
+typedef struct
+{
+    //QSPI periferiara mutato pointer
+    Qspi* hw;
+
+    //Clock Polarity
+    //false: The inactive state value of SPCK is logic level zero.
+    //true:  The inactive state value of SPCK is logic level one.
+    bool cpol;
+
+    //Clock Phase
+    //false: Data is changed on the leading edge of SPCK and captured on the
+    //       following edge of SPCK.
+    //true:  Data is captured on the leading edge of SPCK and changed on the
+    //       following edge of SPCK.
+    bool cpha;
+
+    //baud regiszterbe irando ertek
+    uint32_t baudValue;
+
+    //Delay Between Consecutive Transfers
+    uint32_t dlyBCT;
+    //Minimum Inactive CS Delay
+    uint32_t dlyCS;
+    //Delay Before SCK
+    uint32_t dlySC;
+} MyQSPI_config_t;
+//------------------------------------------------------------------------------
+//MyQSPI valtozoi
+typedef struct
+{
+    //QSPI regisztereire mutato pointer
+    Qspi* hw;
+} MyQSPI_t;
+//------------------------------------------------------------------------------
+//Driver inicializalasa
+void MyQSPI_init(MyQSPI_t* dev, const MyQSPI_config_t* cfg);
+
+//QSPI periferian adat transzfer
+status_t MyQSPI_transfer(MyQSPI_t* dev, const MyQSPI_cmd_t* cmd);
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+#endif //MYQSPI_H_
