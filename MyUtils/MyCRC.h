4eeddd6b915c9ac28c3eada9cd9e84bb3c4de37e
Sütő István <suti1977@gmail.com>
2021-06-10 15:26:43 +0200

    MyCRC modulban a CCIT CRC16 számítás táblázatosra cserélve. MyCRC_calcCCIT_next() lehetővé teszi kezdő értkkel ellátott CRC számítást, és görgetést. MyCRC_calcCCIT_zero() 0 kezdőértékkel rendelkező CCIT16 számítás.

--- MyUtils/MyCRC.h
+++ MyUtils/MyCRC.h
@@ -9,7 +9,17 @@
 #include "MyCommon.h"
 
 
+//CRC szamitas az indulo ertek beallitasaval. Ez lehetove teszi tobb blokk
+//egymas utani szamitasat.
+uint16_t MyCRC_calcCCIT_next(uint16_t initValue,
+                             const uint8_t* data,
+                             uint32_t size);
+
+//CRC szamitas. Poligon: 0x1021  kezdo ertek: 0xffff
 uint16_t MyCRC_calcCCIT(uint8_t* data, uint32_t length);
+
+//CRC szamitas. Poligon: 0x1021  kezdo ertek: 0x0000
+uint16_t MyCRC_calcCCIT_zero(uint8_t* data, uint32_t length);
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 #endif //MYCRC_H_
